027799cafb116809c63b27f7fba55b224b72e720
islam <aitoumessaoudnoureddine@gmail.com>
2024-08-28 18:32:14 +0100

    start

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-alert("hello");
+document.getElementById("joinButton").addEventListener("click", function() {
+    var button = this;
+    button.classList.add("clicked");
+
+    // Remove the animation class after it's done to allow replay
+    setTimeout(function() {
+        button.classList.remove("clicked");
+    }, 400); // Match this duration to the animation duration in CSS
+});
